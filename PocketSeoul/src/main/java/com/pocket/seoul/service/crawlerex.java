48f11060d3107a05ce9a6256748e5f2747fe43be
문혜빈 <strut_@naver.com>
2023-01-26 15:16:17 +0900

    fix : url, date 추가

--- PocketSeoul/src/main/java/com/pocket/seoul/service/crawlerex.java
+++ PocketSeoul/src/main/java/com/pocket/seoul/service/crawlerex.java
@@ -10,10 +10,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.pocket.member.entity.Mark;
-import com.pocket.seoul.entity.Lh;
-import com.pocket.seoul.entity.Sh;
-import com.pocket.seoul.entity.YouthCenter;
-import com.pocket.seoul.entity.youthSeoul;
+
 
 public class crawlerex {
 
@@ -22,100 +19,162 @@ public class crawlerex {
 		
 			List<Mark> list = new ArrayList<>();
 			
-			Document doc = Jsoup.connect("https://youth.seoul.go.kr/site/main/board/notice/list?baCategory1=basic&baCommSelec=true").get();
-			Elements titles =  doc.getElementsByAttributeValue("class", "tlt");
-			Elements url = doc.select(".tlt a");
-			
-			for (int i = 0; i < 5; i++) {
-	            Element title = titles.get(i);
-	            Element urls = url.get(i);
-	            
-	            String titleStr = title.text();
-	            String urlStr = urls.attr("abs:href");
-	            String dateStr = "날짜";
-	            
-	            Mark mark = new Mark(titleStr, urlStr, dateStr);
+			try {
+				
+				Document doc = Jsoup.connect("https://youth.seoul.go.kr/site/main/board/notice/list?baCategory1=basic&baCommSelec=true").get();
+				
+				Elements titles =  doc.getElementsByAttributeValue("class", "tlt");
+				Elements url = doc.select(".tlt a");
+				Elements data = doc.select(".pc");
+				
+				ArrayList<String> dateArr = new ArrayList<String>();
+				
+				for(int i = 4 ; i < 13; i+=2 ) {
+					 Element datas = data.get(i);
+			         String date = datas.text();
+			         dateArr.add(date);
+			        
+				}
+				
+				for (int i = 0; i < 5; i++) {
+		            Element title = titles.get(i);
+		            Element urls = url.get(i);
+		           
+		            String titleStr = title.text();
+		            String urlStr = urls.attr("abs:href");
+		            String dateStr = dateArr.get(i);
+		     
+		            Mark mark = new Mark(titleStr, urlStr, dateStr);
 
-	            list.add(mark);
-	        }
+		            list.add(mark);
+		        }
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 				return list;
 		}
+		
 		
 		public List<Mark> getSh() throws IOException{
 			
 			List<Mark> list = new ArrayList<>();
 			
-			Document doc = Jsoup.connect("https://www.i-sh.co.kr/main/lay2/program/S1T294C295/www/brd/m_241/list.do").get();
-			Elements titles =  doc.getElementsByAttributeValue("class", "txtL");
-			Elements url = doc.select(".txtL a");
-			for (int i = 0; i < 5; i++) {
-				 Element title = titles.get(i);
-				 Element urls = url.get(i);
-				 
-				 String titleStr = title.text();
-				 String urlStr = urls.attr("abs:href");
-				 String dateStr = "날짜";
-				 
-				 Mark mark = new Mark(titleStr, urlStr, dateStr);
-
-				 
-				 // url 절대값 안넘어감 list->view로 바꿔야 됨
-				 list.add(mark);
+			try {
+					
+				Document doc = Jsoup.connect("https://www.i-sh.co.kr/main/lay2/program/S1T294C295/www/brd/m_241/list.do").get();
+				Elements titles =  doc.getElementsByAttributeValue("class", "txtL");
+				Elements url = doc.select(".txtL a");
+				Elements data = doc.select(".num");
+				
+				ArrayList<String> dateArr = new ArrayList<String>();
+				
+				for(int i = 0 ; i < 9; i+=2 ) {
+					 Element datas = data.get(i);
+			         String date = datas.text();
+			         
+			         dateArr.add(date);
+			        
+				}
+				
+				for (int i = 0; i < 5; i++) {
+					 Element title = titles.get(i);
+					 Element urls = url.get(i);
+					 data.get(i);
+					 String titleStr = title.text();
+					 String urlStr = urls.attr("abs:href");
+					 String dateStr = dateArr.get(i);
+					 
+					 Mark mark = new Mark(titleStr, urlStr, dateStr);
+	
+					 list.add(mark);
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			System.out.println(list);
-			return list;
+				return list;
 		}
+		
 		
 		public List<Mark> getLh() throws IOException{
 			
 			List<Mark> list = new ArrayList<>();
-			// System.out.println("LH");
-			Document doc = Jsoup.connect("https://www.lh.or.kr/bbs/list.do?sCode=user&mId=633&mPid=26&bbsSeq=&nttSeq=").get();
-			Elements titles =  doc.getElementsByAttributeValue("class", "subject");
-			// Elements titles = doc.select(".notice_web li a");
-		//	System.out.println(titles);
-			// Elements url = doc.select(".txtL a");
-			for (int i = 0; i < 5; i++) {
-				 Element title = titles.get(i);
+		
+			try {
+				
 			
-				 
-				 String titleStr = title.text();
-				 String urlStr = ("url");
-				 String dateStr = "날짜";
-				 
-				 Mark mark = new Mark(titleStr, urlStr, dateStr);
-
-				 
-				 // url 절대값 안넘어감 list->view로 바꿔야 됨
-				 list.add(mark);
+				Document doc = Jsoup.connect("https://www.lh.or.kr/bbs/list.do?sCode=user&mId=633&mPid=26&bbsSeq=&nttSeq=").get();
+				Elements titles =  doc.getElementsByAttributeValue("class", "subject");
+				Elements data = doc.select("tr td+td+td+td+td");
+				
+				ArrayList<String> dateArr = new ArrayList<String>();
+				
+				for(int i = 0 ; i < 9; i+=2 ) {
+					 Element datas = data.get(i);
+			         String date = datas.text();
+			         System.out.println("date :" + date);
+			         dateArr.add(date);
+			        
+				}
+				
+				
+				for (int i = 0; i < 5; i++) {
+					 Element title = titles.get(i);
+					 Element datas = data.get(i);
+					
+					 
+					 String titleStr = title.text();
+					 String urlStr = ("https://www.lh.or.kr/bbs/list.do?sCode=user&mId=633&mPid=26&bbsSeq=&nttSeq=");
+					 String dateStr = dateArr.get(i);
+					 
+					 Mark mark = new Mark(titleStr, urlStr, dateStr);
+	
+					 
+				
+					 list.add(mark);
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			System.out.println(list);
-			return list;
+
+				return list;
 		}
 		
+		
 		public List<Mark> getYouthCenter() throws IOException{	
+			
 			List<Mark> list = new ArrayList<>();
 			
-			Document doc = Jsoup.connect("https://www.youthcenter.go.kr/jynTips/jynTipsEachList.do?categoryCode=0004&pageIndex=1&tipsId=&actionFlag=&srchKeyAll=&srchValueAll=&srchSortOrder=4&srchKeyEach=&srchValueEach=").get();
-			Elements titles =  doc.getElementsByAttributeValue("class", "txt");
-			for (int i = 0; i < 5; i++) {
-				 Element title = titles.get(i);
-			
-				 
-				 String titleStr = title.text();
-				 String urlStr = ("url");
-				 String dateStr = "날짜";
-				 
-				 Mark mark = new Mark(titleStr, urlStr, dateStr);
-
-				 
-				 // url 절대값 안넘어감 list->view로 바꿔야 됨
-				 list.add(mark);
+			try {
+					
+				Document doc = Jsoup.connect("https://www.youthcenter.go.kr/jynTips/jynTipsEachList.do?categoryCode=0004&pageIndex=1&tipsId=&actionFlag=&srchKeyAll=&srchValueAll=&srchSortOrder=4&srchKeyEach=&srchValueEach=").get();
+				Elements titles = doc.select(".txt strong");
+				Elements data = doc.select(".date");
+				
+				for (int i = 0; i < 5; i++) {
+					 Element title = titles.get(i);
+					 Element datas = data.get(i+1);
+					
+					 String titleStr = title.text();
+					 String urlStr = ("https://www.youthcenter.go.kr/jynTips/jynTipsEachList.do?categoryCode=0004&pageIndex=1&tipsId=&actionFlag=&srchKeyAll=&srchValueAll=&srchSortOrder=4&srchKeyEach=&srchValueEach=");
+					 String dateStr = datas.text();
+					 
+					 Mark mark = new Mark(titleStr, urlStr, dateStr);
+	
+					 
+				
+					 list.add(mark);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			System.out.println(list);
-			return list;
-		}
 			
-		}
+			return list;
+	}
+			
+	}
 		
 
